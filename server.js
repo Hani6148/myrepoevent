04183782cae6424c9ad393a490fa143e5dbd3f7e
hani6148 <hanikhellafi@gmail.com>
2019-09-24 22:16:17 -0500

    first

--- server.js
+++ server.js
@@ -10,7 +10,13 @@ var cookieParser = require("cookie-parser");
 const PORT = process.env.PORT || 3001;
 var session = require("express-session");
 // Define middleware here
-app.use(cors());
+app.use(function(req, res, next) {
+  res.header('Access-Control-Allow-Credentials', true);
+  res.header('Access-Control-Allow-Origin', req.headers.origin);
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+  res.header('Access-Control-Allow-Headers', 'X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept');
+  next();
+});
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(cookieParser());
