bdb7018026a0dd94566769e073400425e372c4bd
Abdel <abdelmounaim.lallouache@gmail.com>
2019-09-30 09:16:28 -0500

    deploy

--- routes/api/googlebooks.js
+++
@@ -1,9 +0,0 @@
-const router = require("express").Router();
-const googleController = require("../../controllers/googlebookController");
-
-// Matches with "/api/books"
-router.route("/")
-  .get(googleController.findAll)
- 
-
-module.exports = router;
