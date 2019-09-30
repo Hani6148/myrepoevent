79d04d5de40d46a2b9f4a40b0d390e92a9338b74
Abdel <abdelmounaim.lallouache@gmail.com>
2019-09-30 09:22:30 -0500

    deploy

--- client/src/App.js
+++ client/src/App.js
@@ -17,7 +17,7 @@ class App extends Component {
         <Switch>
           <Route exact path="/" component={Login} />
           <Route exact path="/signup" component={Signup} />
-          <Route exact path="/feed" component={Main} />
+          <Route exact path="/mainpage" component={Main} />
           <Route component={NoMatch} />
         </Switch>
       </div>
