11b3e6a6a508c06b6d69e35dbc76935b4669569e
Abdel <abdelmounaim.lallouache@gmail.com>
2019-10-01 09:50:37 -0500

    deploy6

---
+++ client/build/precache-manifest.59cfcab5e6857212e842e3dda339f589.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "513785b87c679a13b3e9c4c1a3fc5285",
+    "url": "/static/media/logo.513785b8.png"
+  },
+  {
+    "revision": "326b5e01578ad65355a20b2c662da833",
+    "url": "/static/media/event.326b5e01.JPG"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "16acdd92af158cbc5304",
+    "url": "/static/js/main.9684ea1e.chunk.js"
+  },
+  {
+    "revision": "668b44f00392cf0e0c07",
+    "url": "/static/js/2.778967db.chunk.js"
+  },
+  {
+    "revision": "16acdd92af158cbc5304",
+    "url": "/static/css/main.95948a8c.chunk.css"
+  },
+  {
+    "revision": "90ab7ea08ce3e116833143e43292dead",
+    "url": "/index.html"
+  }
+];
