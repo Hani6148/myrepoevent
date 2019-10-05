8427761fbdc9f3f1aca55b26df57b12e11a68fd9
Abdel <abdelmounaim.lallouache@gmail.com>
2019-10-05 02:50:17 -0500

    comments

--- client/src/components/modal.js
+++ client/src/components/modal.js
@@ -1,15 +1,33 @@
 import React, { Component } from "react";
 import { FaHeart, FaHeartBroken } from "react-icons/fa";
-import { MdInsertComment,MdNavigateNext,MdNavigateBefore } from "react-icons/md";
+import { MdInsertComment, MdNavigateNext, MdNavigateBefore } from "react-icons/md";
+import Axios from "axios"
+
 
 class Modal extends Component {
-    state={
-        user: {}
+    state = {
+        user: {},
+        value: "",
     }
 
-    componentDidMount(){
-        this.setState({user : this.props.user})
+    componentDidMount() {
+        this.setState({ user: this.props.user })
     }
+    handleChange = (event) => {
+        this.setState({ value: event.target.value });
+    }
+    postComment = (e) => {
+        e.preventDefault();
+        Axios.post("/api/comment/create", {
+            text: this.state.value,
+            data: this.props.id,
+            user: this.state.user._id
+        }).then(res =>
+            this.props.getComments()
+        )
+    }
+
+
     render() {
         return (
 
@@ -17,9 +35,9 @@ class Modal extends Component {
                 <div className="modalcontainer modal-dialog-centered" role="document">
                     <div className="modal-content">
                         <div className="modal-header">
-                        <button className="btn btn-outline-secondary" type="button" id="button-addon2"><MdNavigateBefore /></button>
-                        <button className="btn btn-outline-secondary" type="button" id="button-addon2"><MdNavigateNext /></button>
-                            
+                            <button className="btn btn-outline-secondary" type="button" id="button-addon2"><MdNavigateBefore /></button>
+                            <button className="btn btn-outline-secondary" type="button" id="button-addon2"><MdNavigateNext /></button>
+
                             <button type="button" className="close" data-dismiss="modal" aria-label="Close" onClick={() => this.props.hide()}>
                                 <span aria-hidden="true">&times;</span>
                             </button>
@@ -33,26 +51,49 @@ class Modal extends Component {
                                     </div>
                                     <div className="col-5">
                                         <div className="container" id="cmtdiv">
-                                            <h3 className="sectiontitle">Comments</h3>
-                                            <div className="commentsbox">
-                                                <div className="row" id="comments">
-                                                        <div className="col-3">
-                                                            <img src="https://picsum.photos/200" className="friendimg img-thumbnail rounded-circle" alt="..."/>
-                                                        </div>
-                                                        <div className="col-9 d-table">
-                                                        <div class="card card-body d-table-cell align-middle ">
-                                                            <p className="comment">this is the first comment</p>
+                                            <div className="row">
+                                                <h3 className="sectiontitle">Comments</h3>
+                                            </div>
+
+                                            <div className="row" id="commentsrow">
+                                                <div className="container" id="commentscontainer">
+
+                                                {this.props.comments.map(ele => {
+                                                        return(
+                                                            <div className="row" id="commentrow">
+                                                            <div className="col-3" style={{ paddingRight: "0px" }}>
+
+                                                                <img src={ele.user.photo} className="friendimg img-thumbnail rounded-circle" alt="..." />
+
+                                                            </div>
+                                                            <div className="col-9 d-table" style={{ paddingLeft: "0px" }}>
+                                                                <div class="card card-body d-table-cell align-middle " style={{ border: "none" }}>
+                                                                    <p className="comment">{ele.text}</p>
+                                                                </div>
                                                             </div>
                                                         </div>
+                                                        )
+                                                    })}
+                                                    
                                                 </div>
-                                                <div className="row">
-                                                    <div className="input-group mb-3">
-                                                        <input type="text" className="form-control" placeholder="Comments..." aria-label="Comments" aria-describedby="button-addon2" />
-                                                        <div className="input-group-append" />
-                                                        <button className="btn btn-outline-secondary" type="button" id="button-addon2"><MdInsertComment /></button>
-                                                        <button className="btn btn-outline-secondary" type="button" id="button-addon2"><FaHeart /></button>
-                                                        <button className="btn btn-outline-secondary" type="button" id="button-addon2"><FaHeartBroken  /></button>                                                        
-                                                    </div>
+                                            </div>
+                                            <div className="row">
+                                                <div className="input-group mb-3">
+                                                    <input type="text" 
+                                                    className="form-control" 
+                                                    placeholder="Comments..." 
+                                                    aria-label="Comments" 
+                                                    aria-describedby="button-addon2"
+                                                    value={this.state.value}
+                                                    onChange={this.handleChange} />
+                                                    <div className="input-group-append" />
+                                                    <button className="btn btn-outline-secondary" 
+                                                    type="button" 
+                                                    id="button-addon2"
+                                                    onClick={this.postComment}
+                                                    ><MdInsertComment /></button>
+                                                    <button className="btn btn-outline-secondary" type="button" id="button-addon2"><FaHeart /></button>
+                                                    <button className="btn btn-outline-secondary" type="button" id="button-addon2"><FaHeartBroken /></button>
                                                 </div>
                                             </div>
                                         </div>
@@ -63,6 +104,7 @@ class Modal extends Component {
                     </div>
                 </div>
             </div>
+
 
         )
     }
--- client/src/components/modalvid.js
+++ client/src/components/modalvid.js
@@ -3,18 +3,35 @@ import { FaHeart, FaHeartBroken } from "react-icons/fa";
 import { MdInsertComment, MdNavigateNext, MdNavigateBefore } from "react-icons/md";
 import { Player } from 'video-react';
 import "../../node_modules/video-react/dist/video-react.css";
-
+import Axios from "axios"
 
 
 
 class ModalVid extends Component {
-    state={
-        user: {}
+    state = {
+        user: {},
+        value: "",
     }
 
-    componentDidMount(){
-        this.setState({user : this.props.user})
+    componentDidMount() {
+        this.setState({
+            user: this.props.user,
+        })
     }
+    handleChange = (event) => {
+        this.setState({ value: event.target.value });
+    }
+    postComment = (e) => {
+        e.preventDefault();
+        Axios.post("/api/comment/create", {
+            text: this.state.value,
+            data: this.props.id,
+            user: this.state.user._id
+        }).then(res =>
+            this.props.getComments()
+        )
+    }
+
 
     render() {
         return (
@@ -45,23 +62,56 @@ class ModalVid extends Component {
                                     </div>
                                     <div className="col-5">
                                         <div className="container" id="cmtdiv">
-                                            <h3 className="sectiontitle">Comments</h3>
-                                            <div className="commentsbox">
-                                                <div className="row" id="comments">
+                                            <div className="row">
+                                                <h3 className="sectiontitle">Comments</h3>
+                                            </div>
+
+                                            <div className="row" id="commentsrow">
+                                                <div className="container" id="commentscontainer">
+
+                                                    {this.props.comments.map(ele => {
+                                                        return(
+                                                            <div className="row" id="commentrow">
+                                                            <div className="col-3" style={{ paddingRight: "0px" }}>
+
+                                                                <img src={ele.user.photo} className="friendimg img-thumbnail rounded-circle" alt="..." />
+
+                                                            </div>
+                                                            <div className="col-9 d-table" style={{ paddingLeft: "0px" }}>
+                                                                <div class="card card-body d-table-cell align-middle " style={{ border: "none" }}>
+                                                                    <p className="comment">{ele.text}</p>
+                                                                </div>
+                                                            </div>
+                                                        </div>
+                                                        )
+                                                    })}
 
                                                 </div>
-                                                <div className="row">
-                                                    <div className="input-group mb-3">
-                                                        <input type="text" className="form-control" placeholder="Comments..." aria-label="Comments" aria-describedby="button-addon2" />
-                                                        <div className="input-group-append" />
-                                                        <button className="btn btn-outline-secondary" type="button" id="button-addon2"><MdInsertComment /></button>
-                                                        <button className="btn btn-outline-secondary" type="button" id="button-addon2"><FaHeart /></button>
-                                                        <button className="btn btn-outline-secondary" type="button" id="button-addon2"><FaHeartBroken /></button>
-                                                    </div>
+                                            </div>
+                                            <div className="row">
+                                                <div className="input-group mb-3">
+                                                    <input
+                                                        type="text"
+                                                        className="form-control"
+                                                        placeholder="Comments..."
+                                                        aria-label="Comments"
+                                                        aria-describedby="button-addon2"
+                                                        value={this.state.value}
+                                                        onChange={this.handleChange} />
+                                                    <div className="input-group-append" />
+                                                    <button
+                                                        className="btn btn-outline-secondary"
+                                                        type="button"
+                                                        id="button-addon2"
+                                                        onClick={this.postComment}
+                                                    ><MdInsertComment /></button>
+                                                    <button className="btn btn-outline-secondary" type="button" id="button-addon2"><FaHeart /></button>
+                                                    <button className="btn btn-outline-secondary" type="button" id="button-addon2"><FaHeartBroken /></button>
                                                 </div>
                                             </div>
                                         </div>
                                     </div>
+
                                 </div>
                             </div>
                         </div>
