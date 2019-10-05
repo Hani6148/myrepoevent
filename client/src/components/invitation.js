43870f4d1b8400d20f426da44b2a2bef62a8ac35
hani6148 <hanikhellafi@gmail.com>
2019-10-05 08:16:53 -0500

    deployment

--- client/src/components/invitation.js
+++ client/src/components/invitation.js
@@ -102,14 +102,7 @@ class Invitation extends Component {
               </Card.Description>
             </Card.Content>
             <Card.Content extra>
-              <div className='ui two buttons'>
-                <Button basic color='green' onClick={this.acceptInvitation}>
-                  Accpet
-                </Button>
-                <Button basic color='red' onClick={this.acceptInvitation}>
-                  Decline
-                </Button>
-              </div>
+              
             </Card.Content>
           </Card>
           
